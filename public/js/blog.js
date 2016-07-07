d5c79fa569bca96d7c61afe240814e41fbb3a8d4
Sylwia Michon <smichon@benchmarkeducation.com>
2016-07-07 16:10:01 -0400

    create css file and js file with Vue model for blog home page

---
+++ public/js/blog.js
@@ -0,0 +1,97 @@
+
+// console.log('hello');
+// 
+new Vue({
+	el: '#guestbook',
+	data: {
+		name2: 'Test',
+		newMessage: { name: '', message: ''},
+		formNotFilled: false,
+		submitted: false
+	},
+	ready : function() {       
+        console.log('vueModel is ready');           
+        this.fetchMessages();
+
+     //    console.log('after fetchMessages');
+    	// console.log(this.messages);
+    },
+	// filters: {
+	// 	orderBy: function(){
+
+	// 	}
+	// },
+	computed: {
+		formNotFilled: function() {
+			for (var key in this.newMessage) {
+				if (! this.newMessage[key]) return true;
+			}
+			return false;
+		}
+	},
+	methods: {
+		// fetchMessages: function() {
+  //       	console.log('fetchMessages()');
+  //       	inputData = { name: 'my name', message: 'my message' };
+  //       	this.$http.get('api/messages', inputData, function(response) {
+  //       		console.log('response');
+  //       		console.log(response);
+  //       		this.$set('messages', response);
+  //       	});
+  //       	console.log('messages after');
+  //       	console.log(this.messages);
+		// }
+		fetchMessages: function () {
+            console.log('fetchMessages');
+            // this.$http.get('api/messages', function (responseData) {
+            // 	console.log('responseData:');
+            // 	console.log(responseData);
+            //     temp = responseData;
+            // });
+
+        	$.ajax({
+		        url: 'api/messages',
+		        type: 'get',
+		        async: false,
+		        success: function(responseData) {
+		        	// console.log('responseData:');
+           //  		console.log(responseData);
+            		temp = responseData;
+		        }
+			});
+            this.$set('messages', temp);
+
+        },
+        addMessage: function(e) {
+        	e.preventDefault();
+        	this.messages.push(this.newMessage);
+        	inputData = this.newMessage;
+        	$.ajax({
+		        url: 'pi/messages/create',
+		        inputData,
+		        type: 'post',
+		        async: false,
+		        success: function(responseData) {
+		        	// console.log('responseData:');
+           //  		console.log(responseData);
+            		temp = responseData;
+		        }
+			});
+        	this.newMessage = { name: '', message: ''};
+
+        	this.submitted = true;
+        	
+        }
+		// createNewMessage: function(e) {
+		// 	e.preventDefault();
+
+		// 	inputData = { name: 'my name', message: 'my message' };
+			
+		// 	this.$http.post('api/messages/create', inputData, function(response) {
+		// 		console.log(response);
+  //       	});
+		// }
+	}
+})
+
+// console.log('bye2');
